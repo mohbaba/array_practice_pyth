0faf61402154c82b7213cec147e7a3cd603165d8
Muhammad Baba Muhammad <59345145+mohbaba@users.noreply.github.com>
2023-12-31 18:18:42 +0100

    Update EvenPositionTest.java

--- EvenPositionTest.java
+++ EvenPositionTest.java
@@ -7,34 +7,34 @@ public class EvenPositionTest{
 
 	@Test
 	public void testThatEvenPositionCheckerExists(){
-		EvenPositionChecker evenPositionChecker = new EvenPositionChecker();
+		EvenPositionChecker positionChecker = new EvenPositionChecker();
 
 	}
 
 	@Test
 	public void testIntEvenPositionChecker(){
-		EvenPositionChecker evenPositionChecker = new EvenPositionChecker();
+		EvenPositionChecker positionChecker = new EvenPositionChecker();
 		int[] array = {1,2,3,4,5,6,7,8,9,10};
 		int[] expected = {2,4,6,8,10};
-		assertArrayEquals(expected, evenPositionChecker.intPositionChecker(array));
+		assertArrayEquals(expected, positionChecker.intPositionChecker(array));
 
 	}
 
 	@Test
 	public void testStringPositionChecker(){
-		EvenPositionChecker evenPositionChecker = new EvenPositionChecker();
+		EvenPositionChecker positionChecker = new EvenPositionChecker();
 		String[] array = {"Moh","Baba","Dee","Daddy"};
 		String[] expected = {"Baba","Daddy"};
-		assertArrayEquals(expected, evenPositionChecker.stringPositionChecker(array));
+		assertArrayEquals(expected, positionChecker.stringPositionChecker(array));
 		
 	}
 
 	@Test
 	public void testObjectPositionChecker(){
-		EvenPositionChecker evenPositionChecker = new EvenPositionChecker();
+		EvenPositionChecker positionChecker = new EvenPositionChecker();
 		Object[] array = {"Moh",1,"Dee",20.9};
 		Object[] expected = {1,20.9};
-	 assertArrayEquals(expected,evenPositionChecker.objectPositionChecker(array));
+	 assertArrayEquals(expected,positionChecker.objectPositionChecker(array));
 
 	}
 
